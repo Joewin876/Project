8aa3207c99f34e659ef7f97bf8b0cfc4ae9faf47
Joewin876 <117723278+Joewin876@users.noreply.github.com>
2022-11-08 23:04:32 +0800

    Update MengkonversiRupiahKeUsdDanEuro.java

--- Mengkonversi rupiah ke usd dan euro/src/mengkonversi/rupiah/ke/usd/dan/euro/MengkonversiRupiahKeUsdDanEuro.java
+++ Mengkonversi rupiah ke usd dan euro/src/mengkonversi/rupiah/ke/usd/dan/euro/MengkonversiRupiahKeUsdDanEuro.java
@@ -1,37 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mengkonversi.rupiah.ke.usd.dan.euro;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Joewi
- */
+
 public class MengkonversiRupiahKeUsdDanEuro {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        String tanya;
+       
         Scanner input = new Scanner(System.in);
-        System.out.println("Apakah anda lapar?(Ya atau Tidak) : " );
-        tanya = input.next();
-        if ("Ya".equalsIgnoreCase(tanya))
-        {
-            System.out.println("Makan");
-            
-        }
-        else
-        {
-            System.out.println("Tidak Makan");
-          
-        }
+        double usd, euro, rupiah;        
+        
+        
+        System.out.println("masukan rupiah = ");
+        rupiah=input.nextInt();
+        usd = rupiah/14508;
+        euro = rupiah/17064;
+        System.out.println("USD = "+usd);
+        System.out.println("Euro = "+euro);
+        
                 
     }
     
